6fa074dc6f587d37ded23dbbe2fd1cdf63e81918
PabloDiaz <peiibol1017@gmail.com>
2022-03-15 18:42:52 +0100

    peticiones de hora fecha y precio, aun medio mal

--- peticion.js
+++ peticion.js
@@ -11,19 +11,67 @@ async function getPrice(Url){
 
     const info = await data.json();
 
-    const electricPrice = JSON.parse(info.contents)
+    const electricPrice = JSON.parse(info.contents);
 
-    return electricPrice.price;
+    return electricPrice.price
         }
         catch(e){console.log(e.message)}
     }
 
-console.log(getPrice(apiUrl));
-console.log(getPrice(maxUrl));
-console.log(getPrice(minUrl));
+    const actualPrice = getPrice(apiUrl);
+    const maxPrice = getPrice(maxUrl);
+    const minPrice = getPrice(minUrl);
+    
+    console.log(actualPrice);
+    console.log(maxPrice);
+    console.log(minPrice);
 
-const actualPrice = getPrice(apiUrl);
-const maxPrice = getPrice(maxUrl);
-const minPrice = getPrice(minUrl);
+async function getHour(Url){
+try{
 
+    const data = await fetch(Url);
+
+    const info = await data.json();
+
+    const electricHour = JSON.parse(info.contents);
+
+    return electricHour.hour
+
+        }
+        catch(e){console.log(e.message)}
+    }
+
+
+const actualHour = getHour(apiUrl);
+const maxHour = getHour(maxUrl);
+const minHour = getHour(minUrl);
+
+console.log(actualHour);
+console.log(maxHour);
+console.log(minHour);
+
+async function getDate(Url){
+
+try{
+
+    const data = await fetch(Url);
+
+    const info = await data.json();
+
+    const electricDate = JSON.parse(info.contents);
+
+    return electricDate.date
+
+        }
+        catch(e){console.log(e.message)}
+    }
+
+
+const actualDate = getDate(apiUrl);
+const maxDate = getDate(maxUrl);
+const minDate = getDate(minUrl);
+
+console.log(actualDate);
+console.log(maxDate);
+console.log(minDate);
 
