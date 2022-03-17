5feead045e998ec353ab57ff363c1dffa06aeb98
Ruben <crodriguez.ruben@gmail.com>
2022-03-17 21:49:42 +0100

    modificación de main.js y calculo.js

--- javascript/calculo.js
+++ javascript/calculo.js
@@ -2,16 +2,10 @@ import { test } from "./peticion.js"
 
 //Aquí habría que poner el precio de la petición
 
-
-export const dispositivo1 = ((260 / 1000000) * (await test("actual", "price"))).toFixed(3);
-export const dispositivo2 = ((45 / 1000000) * (await test("actual", "price"))).toFixed(3);
-export const dispositivo3 = ((459/1000000)* (await test("actual", "price"))).toFixed(3);
-export const dispositivo4 = ((85/1000000)* (await test("actual", "price"))).toFixed(3);
-
-export const dispositivo5 = ((4.34/1000)* (await test("actual", "price"))).toFixed(3);
-export const dispositivo6 = ((0.025/1000)* (await test("actual", "price"))).toFixed(3);
-export const dispositivo7 = ((7.4/1000)* (await test("actual", "price"))).toFixed(3);
-export const dispositivo8 = ((0.97/1000)* (await test("actual", "price"))).toFixed(3);
+export async function calcularPrecio (power) {
+    const peticion = await test("actual", "price");
+    return ((power/1000) *peticion)
+}
 
 
 
--- javascript/main.js
+++ javascript/main.js
@@ -1,34 +1,10 @@
-import {dispositivo1, dispositivo2, dispositivo3, dispositivo4, dispositivo5, dispositivo6, dispositivo7, dispositivo8} from "./calculo.js";
+import {calcularPrecio} from "./calculo.js";
 import {test} from "./peticion.js";
 
 //Aquí en el main manipularía el DOM. Para colocar cosas en el DOM tendría que poner funciones con callbacks a funciones de peticiones.
 
-
 //Selectores
-const euroMvatioHora = document.querySelector("#valor_precio_luz");
-const consumoSobremesa = document.querySelector("#sobremesa p:last-child");
-const consumoMonitor = document.querySelector("#Monitor p:last-child");
-const consumoImpresora = document.querySelector("#Impresora p:last-child");
-const consumoPortatil = document.querySelector("#Portatil p:last-child");
-//----------------------------------------------------------------------------------------------------
-const consumoLavadora = document.querySelector("#Lavadora p:last-child");
-const consumoFrigorifico = document.querySelector("#Frigorifico p:last-child");
-const consumoInduccion = document.querySelector("#Induccion p:last-child");
-const consumoHorno = document.querySelector("#Horno p:last-child");
-
-
-//Resultados cálculo
-const sobremesa = dispositivo1;
-const monitorSobremesa = dispositivo2; 
-const impresora = dispositivo3;
-const portatil = dispositivo4;
-const lavadora = dispositivo5;
-const frigorifico = dispositivo6;
-const induccion = dispositivo7;
-const horno = dispositivo8;
-
-
-
+const arrayPower = document.querySelectorAll("[data-power]");
 
 //Funciones
   async function showActualPrice(dom, B, A) {
@@ -36,22 +12,12 @@ const horno = dispositivo8;
     dom.textContent = `${price} €/Mwh`;
   }
   
-  function showAnPrice(dom, value) {
-    dom.textContent = `${value} €`;
+  async function setPrice () {
+    for (let i=0; i<arrayPower.length; i++) {
+      const indexValue = arrayPower[i].dataset.power
+      const devicePrice = await calcularPrecio(indexValue)
+      arrayPower[i].textContent = `${(Math.round(devicePrice*100))/100} €`
+    }
   }
-  
 
-  //Llamadas para la página
-  showActualPrice(euroMvatioHora, "actual", "price");
-  
-  showAnPrice(consumoSobremesa, sobremesa);
-  showAnPrice(consumoMonitor, monitorSobremesa);
-  showAnPrice(consumoImpresora, impresora);
-  showAnPrice(consumoPortatil, portatil);
-  
-  showAnPrice(consumoLavadora, lavadora);
-  showAnPrice(consumoFrigorifico, frigorifico);
-  showAnPrice(consumoInduccion, induccion);
-  showAnPrice(consumoHorno, horno);
-
-
+setPrice ()
