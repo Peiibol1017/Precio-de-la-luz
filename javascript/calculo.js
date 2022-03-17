d7bf8700bc9dc1ffaa7c01a3df703fcd7e5552f1
Ruben <crodriguez.ruben@gmail.com>
2022-03-17 16:23:53 +0100

    Finalizado:Informatica&Cocina; Falta: comprobación

--- javascript/calculo.js
+++ javascript/calculo.js
@@ -1,18 +1,18 @@
-import { text } from "./peticion.js"
+import { test } from "./peticion.js"
 
 //Aquí habría que poner el precio de la petición
 
 
-export const dispositivo1 = (250 / 1000000) * (await test("actual", "price"));
-// const dispositivo2 = ((/* Seleccionar etiqueta *//1000)*currentPrice.price)
-// const dispositivo3 = ((/* Seleccionar etiqueta *//1000)*currentPrice.price)
-// const dispositivo4 = ((/* Seleccionar etiqueta *//1000)*currentPrice.price)
-// const dispositivo5 = ((/* Seleccionar etiqueta *//1000)*currentPrice.price)
-// const dispositivo6 = ((/* Seleccionar etiqueta *//1000)*currentPrice.price)
-// const dispositivo7 = ((/* Seleccionar etiqueta *//1000)*currentPrice.price)
-// const dispositivo8 = ((/* Seleccionar etiqueta *//1000)*currentPrice.price)
-// const dispositivo9 = ((/* Seleccionar etiqueta *//1000)*currentPrice.price)
-// const dispositivo100 = ((/* Seleccionar etiqueta *//1000)*currentPrice.price)
+export const dispositivo1 = ((260 / 1000000) * (await test("actual", "price"))).toFixed(3);
+export const dispositivo2 = ((45 / 1000000) * (await test("actual", "price"))).toFixed(3);
+export const dispositivo3 = ((459/1000000)* (await test("actual", "price"))).toFixed(3);
+export const dispositivo4 = ((85/1000000)* (await test("actual", "price"))).toFixed(3);
+
+export const dispositivo5 = ((4.34/1000)* (await test("actual", "price"))).toFixed(3);
+export const dispositivo6 = ((0.025/1000)* (await test("actual", "price"))).toFixed(3);
+export const dispositivo7 = ((7.4/1000)* (await test("actual", "price"))).toFixed(3);
+export const dispositivo8 = ((0.97/1000)* (await test("actual", "price"))).toFixed(3);
+
 
 
 
--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,4 @@
-import {dispositivo1} from "./calculo.js";
+import {dispositivo1, dispositivo2, dispositivo3, dispositivo4, dispositivo5, dispositivo6, dispositivo7, dispositivo8} from "./calculo.js";
 import {test} from "./peticion.js";
 
 //Aquí en el main manipularía el DOM. Para colocar cosas en el DOM tendría que poner funciones con callbacks a funciones de peticiones.
@@ -6,20 +6,27 @@ import {test} from "./peticion.js";
 
 //Selectores
 const euroMvatioHora = document.querySelector("#valor_precio_luz");
-const consumoXbox = document.querySelector("#vitro p:last-child");
+const consumoSobremesa = document.querySelector("#sobremesa p:last-child");
+const consumoMonitor = document.querySelector("#Monitor p:last-child");
+const consumoImpresora = document.querySelector("#Impresora p:last-child");
+const consumoPortatil = document.querySelector("#Portatil p:last-child");
+//----------------------------------------------------------------------------------------------------
+const consumoLavadora = document.querySelector("#Lavadora p:last-child");
+const consumoFrigorifico = document.querySelector("#Frigorifico p:last-child");
+const consumoInduccion = document.querySelector("#Induccion p:last-child");
+const consumoHorno = document.querySelector("#Horno p:last-child");
 
 
+//Resultados cálculo
+const sobremesa = dispositivo1;
+const monitorSobremesa = dispositivo2; 
+const impresora = dispositivo3;
+const portatil = dispositivo4;
+const lavadora = dispositivo5;
+const frigorifico = dispositivo6;
+const induccion = dispositivo7;
+const horno = dispositivo8;
 
-
-
-
-
-/* Cambios en DOM:
-euroMvatioHora
-ordenadorSobremesa
-consumoPortatil
-consumoMonitor
-consumoServidor */
 
 
 
@@ -29,11 +36,22 @@ consumoServidor */
     dom.textContent = `${price} €/Mwh`;
   }
   
-  async function showAnPrice(dom, value) {
+  function showAnPrice(dom, value) {
     dom.textContent = `${value} €`;
   }
   
 
   //Llamadas para la página
   showActualPrice(euroMvatioHora, "actual", "price");
-  showAnPrice(consumoXbox, xbox);
+  
+  showAnPrice(consumoSobremesa, sobremesa);
+  showAnPrice(consumoMonitor, monitorSobremesa);
+  showAnPrice(consumoImpresora, impresora);
+  showAnPrice(consumoPortatil, portatil);
+  
+  showAnPrice(consumoLavadora, lavadora);
+  showAnPrice(consumoFrigorifico, frigorifico);
+  showAnPrice(consumoInduccion, induccion);
+  showAnPrice(consumoHorno, horno);
+
+
